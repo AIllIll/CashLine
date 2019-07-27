366eff978d16eff3b4403ec07fd6942eb7e39222
AIllIll <864147974@qq.com>
2019-07-27 19:28:15 +0800

    CashLine Initiation

---
+++ app/src/main/java/com/wyc/cashline1/MainActivity.java
@@ -0,0 +1,71 @@
+package com.wyc.cashline1;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.MenuItem;
+import android.view.View;
+
+import com.wyc.cashline1.adapters.SectionsPagerAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.view_pager)
+    ViewPager viewPager;
+    @BindView(R.id.tabs)
+    TabLayout tabs;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        viewPager.setAdapter(sectionsPagerAdapter);
+        tabs.setupWithViewPager(viewPager);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+        = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                Snackbar.make(fab, "navigation_home", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+                return true;
+            case R.id.navigation_dashboard:
+                Snackbar.make(fab, "navigation_dashboard", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                return true;
+            case R.id.navigation_notifications:
+                Snackbar.make(fab, "navigation_notifications", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                return true;
+        }
+        return false;
+        }
+    };
+}
